bf09caf59a14e2111668b266cadff75763652266
Renan Vieira <renanvieira@live.com>
2013-08-04 20:34:05 -0300

    added file to test post notification simulator

--- lib/mundipagg/simulator/test.rb
+++ lib/mundipagg/simulator/test.rb
@@ -1,3 +1,3 @@
 require_relative "PostNotificationSender.rb"
 
-puts Mundipagg::Simulator::PostNotification.SendPostWithRandomData('http://localhost:3000/home/index', :boleto)
+puts Mundipagg::Simulator::PostNotification.SendPostWithRandomData('http://localhost:3000/home/index', :credit)
